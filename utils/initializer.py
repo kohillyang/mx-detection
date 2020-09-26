7f8ff906267608a70ec80614fb905b5631eea6a8
kohill <kohillyang@126.com>
2020-09-26 13:39:29 +0800

    update kaming_uniform

--- utils/initializer.py
+++ utils/initializer.py
@@ -1,11 +1,12 @@
 from mxnet.initializer import register, Initializer
 import mxnet as mx
 import numpy as np
+import torch
 @register
 class KaMingUniform(Initializer):
     def __init__(self):
         super(KaMingUniform, self).__init__()
 
     def _init_weight(self, _, arr):
-        fan_in = arr.shape[1]
-        mx.nd.random.uniform(-1 *np.sqrt(6 / fan_in), np.sqrt(6 / fan_in), shape=arr.shape, out=arr)
+        w = torch.nn.init.kaiming_uniform_(torch.zeros(size=arr.shape), a=1).numpy()
+        arr[:] = mx.nd.array(w)
