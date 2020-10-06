3615dac08b720423ae17275fed064577a6b98cb3
kohill <kohillyang@126.com>
2020-10-06 11:04:36 +0800

    Do not set pool type.

--- scripts/train_retinanet.py
+++ scripts/train_retinanet.py
@@ -362,7 +362,6 @@ def parse_args():
 
 def main():
     os.environ["MXNET_CUDNN_AUTOTUNE_DEFAULT"] = "0"
-    os.environ["MXNET_GPU_MEM_POOL_TYPE"] = "Unpooled"
     load_mobula_ops()
 
     args = parse_args()
